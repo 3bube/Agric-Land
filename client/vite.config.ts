470c4bfe5b5d01195a2c7e32e5c72dd091eb5bd8
Ebubechukwu Ugbor <ugbore2@gmail.com>
2024-12-21 10:45:07 +0100

    changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,29 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
-    }
-  }
 });
